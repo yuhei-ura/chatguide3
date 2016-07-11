180ff985041b31ea450e38ae000f83b62eb2d6f2
Yuhei Urabe <Yuhei Urabe>
2016-07-11 15:17:10 +0900

    set on production.rb
    config.action_cable.allowed_request_origins = [ /https?:\/\/.*/ ]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.serve_static_assets = true
+  config.action_cable.allowed_request_origins = [ /https?:\/\/.*/ ]
 end
